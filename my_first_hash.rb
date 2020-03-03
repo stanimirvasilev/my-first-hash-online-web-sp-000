fa9796a85048a5f22248151a423101de43d6a9cf
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-03 17:30:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,6 +43,6 @@ shipping_manifest["muskets"] = "2"
 shipping_manifest["gun powder"] = "4"
 
   # return the shipping_manifest hash below
-  return the_manifest["oil paintings"]
+  return shipping_manifest
 
 end
